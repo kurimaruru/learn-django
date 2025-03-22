af600f31e2edee8e194cb6b71cdf19c52a45a38a
kurimaruru <kurikuribaseball530@gmail.com>
2025-03-22 21:51:05 +0900

    doc: コメント追加

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
+    # /blog
     path("", views.IndexView.as_view(), name="index"),
+    # /blog/post_list
     path("post_list", views.PostListView.as_view(), name="post_list"),
 ]
