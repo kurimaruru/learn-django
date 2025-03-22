fb9a045532cbcc72cfd90b90810907b0494e0419
kurimaruru <kurikuribaseball530@gmail.com>
2025-03-22 21:41:55 +0900

    feat: generic.ListViewを追加
     - generic.ListView では template のファイル名をルールに沿った形にしてあげることで、明示しなくても呼び出してくれる
     - {model}_{list}.html

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path("", views.IndexView.as_view(), name="index"),
+    path("post_list", views.PostListView.as_view(), name="post_list"),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,11 @@
-from django.views.generic import TemplateView
+from django.views import generic
+from .models import Post
 
-class IndexView(TemplateView):
-    template_name = 'blog/index.html'
+
+class IndexView(generic.TemplateView):
+    template_name = "blog/index.html"
+    model = Post  # 一覧表示させたいモデルを呼び出し
+
+
+class PostListView(generic.ListView):  # generic の ListViewクラスを継承
+    model = Post  # 一覧表示させたいモデルを呼び出し
