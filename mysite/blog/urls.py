ec9f85c5ce0ebf339d2470caabc6e5b40bf1c526
kurimaruru <kurikuribaseball530@gmail.com>
2025-03-22 23:12:57 +0900

    feat: detail , update, delete画面の追加

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path("post_list", views.PostListView.as_view(), name="post_list"),
     # /blog/post_create
     path("post_create", views.PostCreateView.as_view(), name="post_create"),
+    path("post_detail/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("post_update/<int:pk>/", views.PostUpdateView.as_view(), name="post_update"),
+    path("post_delete/<int:pk>/", views.PostDeleteView.as_view(), name="post_delete"),
 ]
