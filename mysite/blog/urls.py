1980dffa219b5c003678a7db0a56f1147b08fec7
kurimaruru <kurikuribaseball530@gmail.com>
2025-03-22 23:02:35 +0900

    feat: create機能の追加

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     # /blog/post_list
     path("post_list", views.PostListView.as_view(), name="post_list"),
+    # /blog/post_create
+    path("post_create", views.PostCreateView.as_view(), name="post_create"),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import Post
+from .forms import PostCreateForm  # forms.py で作ったクラスをimport
+from django.urls import reverse_lazy
 
 
 class IndexView(generic.TemplateView):
@@ -9,3 +11,9 @@ class IndexView(generic.TemplateView):
 
 class PostListView(generic.ListView):  # generic の ListViewクラスを継承
     model = Post  # 一覧表示させたいモデルを呼び出し
+
+
+class PostCreateView(generic.CreateView):  # 追加
+    model = Post  # 作成したい model を指定
+    form_class = PostCreateForm  # 作成した form クラスを指定
+    success_url = reverse_lazy("blog:post_list")  # 記事作成に成功した時のリダイレクト先を指定
